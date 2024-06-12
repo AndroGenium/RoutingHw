f3b9a3bde639b7a85efd66640cf4ba2aac0feccc
AndroGenium <chilachava.doc@gmail.com>
2024-06-12 17:03:28 +0400

    zd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { ContactpageComponent } from './components/contactpage/contactpage.component';
+import { AboutuspageComponent } from './components/aboutuspage/aboutuspage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomepageComponent},
+  {path:'Contact',component: ContactpageComponent},
+  {path:'AboutUs',component: AboutuspageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { ContactpageComponent } from './components/contactpage/contactpage.component';
+import { AboutuspageComponent } from './components/aboutuspage/aboutuspage.component';
+import { PortalComponent } from './components/portal/portal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactpageComponent,
+    AboutuspageComponent,
+    PortalComponent
   ],
   imports: [
     BrowserModule,
